ed59271a834080c01fad06b521800c946eb7c8aa
web2sign <roy@straightarrow.com.ph>
2013-11-25 15:23:58 +0800

    up

--- neckshoulder.php
+++ neckshoulder.php
@@ -18,7 +18,11 @@ color:#CCB363;
 	font-size: 12px;
 	text-decoration:none
 }
-
+.customContentStyle { }
+.customContentStyle h3 { margin-bottom:5px; }
+.customContentStyle p { line-height:150%; margin-bottom:16px; }
+.customContentStyle ol { padding-left:30px; }
+.customContentStyle ol li { margin-bottom:5px; }
 </style>
 
 </head>
@@ -49,11 +53,10 @@ color:#CCB363;
              <h1>Neck and Shoulder Pain</h1>
             </div>
             <br><br>
-            <div class="about_body_text_box">
+            <div class="about_body_text_box"><div class="body_left_text_area customContentStyle">
               <h3><a href="bodymap.php">Your Pain</a>  >> Q magnet Prescription for neck and shoulder pain:</h3>
-              <p class="centeredImageLink">
-                <a href="images/Slide4.jpg">
-                  <img src="images/NS-3device.gif"><br>
+              <p class="centered">
+                <a href="images/Slide4.jpg"><img src="images/NS-3device.gif"><br />
                   Click on image for more details.
                 </a> 
               </p>
@@ -68,9 +71,9 @@ color:#CCB363;
             To find the first and second placements at the base of the neck easily, place them to the right and left of the midline at the points that are tender when pressed. The  third device  is placed approximately 50mm (2 inches) above the bulge at the base of the neck at the 4th cervical vertebra. For most people, this level is usually the hairline.
           </p>
            
-          <iframe width="420" height="315" src="http://www.youtube.com/embed/qpwKh-tYUvQ?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
+          <p class="centered"><iframe width="420" height="315" src="http://www.youtube.com/embed/qpwKh-tYUvQ?feature=player_detailpage" frameborder="0" allowfullscreen></iframe></p>
           
-          <p align="center">
+          <p class="centered">
             <a href="images/Slide5.jpg"><img src="images/NS-4device.gif">
             <br>
             Click on image for more details</a>
@@ -85,55 +88,38 @@ color:#CCB363;
             <a href="http://qmagnets.com/blog/category/pain-testimonies/neck-pain" class="links">
               Click here for Q magnet testimonies for neck pain.
             </a>
-          <table class="tablelist1">
+          <table class="tablelist1" width="100%">
             <thead><tr>
-              <td>Pain Level </td>
-              <td>Order Quantity</td>
-              <td>Shopping Cart</td>
+              <td><span>Pain Level </span></td>
+              <td><span>Order Quantity</span></td>
+              <td><span>Shopping Cart</span></td>
             </tr>
             </thead>
             <tbody>
               <tr>
-                <td>Severe </td>
-                <td>2 X QF28-3 + 2 X QF20-3 (4 device array)</td>
-                <td>$184.00 <a href="http://www.1shoppingcart.com/app/netcart.asp?MerchantID=96291&amp;ProductID=3366213"><img width="134" height="23" border="0" alt="Add to shopping cart" src="images/cart.jpg"></a></td>
+                <td><span>Severe </span></td>
+                <td><span>2 X QF28-3 + 2 X QF20-3 (4 device array)</span></td>
+                <td><span><a href="http://www.1shoppingcart.com/app/netcart.asp?MerchantID=96291&amp;ProductID=3366213"><img width="134" height="23" border="0" alt="Add to shopping cart" src="images/cart.jpg"></a> $184.00</span></td>
               </tr>
               <tr>
-                <td>Severe </td>
-                <td>1 X QF28-3 + 2 X QF20-3 (3 device array)</td>
-                <td>$125.00 <a href="http://www.1shoppingcart.com/app/netcart.asp?MerchantID=96291&amp;ProductID=3366224"><img width="134" height="23" border="0" alt="Add to shopping cart" src="images/cart.jpg"></a></td>
+                <td><span>Severe </span></td>
+                <td><span>1 X QF28-3 + 2 X QF20-3 (3 device array)</span></td>
+                <td><span><a href="http://www.1shoppingcart.com/app/netcart.asp?MerchantID=96291&amp;ProductID=3366224"><img width="134" height="23" border="0" alt="Add to shopping cart" src="images/cart.jpg"></a> $125.00</span></td>
               </tr>
               <tr>
-                <td>Moderate</td>
-                <td>3 X QF20-3</td>
-                <td>$99.00 <a href="http://www.1shoppingcart.com/app/netcart.asp?MerchantID=96291&amp;ProductID=3366228"><img width="134" height="23" border="0" alt="Add to shopping cart" src="images/cart.jpg"></a></td>
+                <td><span>Moderate</span></td>
+                <td><span>3 X QF20-3</span></td>
+                <td><span><a href="http://www.1shoppingcart.com/app/netcart.asp?MerchantID=96291&amp;ProductID=3366228"><img width="134" height="23" border="0" alt="Add to shopping cart" src="images/cart.jpg"></a> $99.00 </span></td>
               </tr>
             </tbody>
             <tfoot>
               <tr>
-                <td colspan="3">For best value purchase the <a href="products.php#QBP">Q
-                  Bonus Package</a></td>
+                <td colspan="3"><span>For best value purchase the <a href="products.php#QBP">Q Bonus Package</a></span></td>
               </tr>
             </tfoot>
           </table>
-          <h3>Note:</h3>
-          <p>These are recommendations from neurologists and physiotherapists who have used
-            Q magnets extensively in a clinical setting. See the <a href="products.php">Q magnets products page</a> for a description of each device, including the sizes and penetration depths. </p>
-          <p>It is important  to get the right dose of magnetic field to the target tissue,  otherwise there will be little or no effect. This may require using the information supplied with your order and on this website with a bit of trial and error to achieve the optimal placement. The best results are usually achieved by seeing an appropriately trained health professional such as a doctor or physiotherapist using their own clinical reasoning and neural anatomy to find the most effective placements. </p>
-          <p>As a general rule,  the larger  devices such as the <a href="buy-QF28-6.php">QF28-6</a> and <a href="buy-QF28-3.php">QF28-3</a> should be used centrally over the spine and over the larger deeper nerves
-            such as the sciatic nerve, the smaller Q magnets such as the <a href="buy-QF20-3.php">QF20-3</a> and <a href="buy-QF15-2.php">QF15-2</a> should be used peripherally over areas such as the elbow and wrist and the smallest models <a href="buy-QF10-2.php">QF10-2</a>, <a href="buy-Q6-15.php">Q6-1.5</a> used on trigger points. With each pain episode, you may require a larger Q
-            magnet and as the pain diminishes substitute with a smaller device to maintain your pain free state.</p>
-          <p>The smaller devices such as the <a href="buy-QF20-2.php">QF20-2</a> have a lower profile, hence 
-            may be more comfortable for daily use. The <a href="buy-Q6-15.php">Q6-1.5</a> is very small 
-            and only requires a small round plaster to apply it to the body, hence it
-            is very comfortable and inconspicuous to wear, but has a shallow penetration and
-            a very small active surface area and is unlikely to be effective over the lower back or neck or the larger joints such as the hip, knee, shoulder, ankle and elbow. </p>
-          <p>The largest Q magnet, the <a href="buy-OF50-3.php">OF50-3</a> has
-            the advantage of covering a  larger active
-            surface area (the active surface area of the Q magnet is  the boundary line between
-            the alternating poles, this is where the magnetic field gradient
-            is at its steepest) and penetrates to around 35mm or 1.4'. Whereas the <a href="buy-QF28-6.php">QF28-6</a> and <a href="buy-QF28-3.php">QF28-3</a> models cannot cover two adjacent spinal levels such as L4/5 and L5/S1 since they have to be separated by an inch or 2.5cm, the <a href="buy-OF50-3.php">OF50-3</a> can cover two adjacent levels which can make all the difference if required. </p>
-              
+          <?php include('includes/note.php'); ?>
+             </div>
              </div>
             <div class="left_bottom_img"><img src="images/body_left_bottom_img.jpg" alt="" border="0"></div>
             </div>
